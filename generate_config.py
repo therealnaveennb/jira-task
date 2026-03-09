5fb4b55fc020365b48dc3097b1295d4f369ea864
Naveen bharathi <95202935+therealnaveennb@users.noreply.github.com>
2026-03-09 19:09:49 +0530

    Create script to generate credentials.ini from template
    This script generates a configuration file 'credentials.ini' using user data from environment variables and a Jinja2 template.

---
+++ generate_config.py
@@ -0,0 +1,34 @@
+import json
+import os
+from jinja2 import Template
+
+# Load data from environment
+user_data = json.loads(os.environ.get('USER_DATA_JSON', '[]'))
+
+template_str = """[DEFAULT]
+jira_domain = {{ jira_domain }}
+board_id = {{ board_id }}
+project_key = {{ project_key }}
+aws_region = {{ aws_region }}
+sender_email = {{ sender_email }}
+
+{% for user in users %}
+[{{ user.name }}]
+jira_email = {{ user.email }}
+jira_api_token = {{ user.token }}
+recipient_email = {{ user.recipient }}
+{% endfor %}
+"""
+
+template = Template(template_str)
+rendered_config = template.render(
+    jira_domain="mitsogo",
+    board_id="21",
+    project_key="DevOps",
+    aws_region="ap-south-1",
+    sender_email=os.environ.get('SENDER_EMAIL'),
+    users=user_data
+)
+
+with open("credentials.ini", "w") as f:
+    f.write(rendered_config)
